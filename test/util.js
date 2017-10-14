94a5d2f6d77f7e1f181ba7e9942307f969c71c09
George Spasov <spasov05203@gmail.com>
2017-10-14 22:23:02 +0300

    Added expectThrow Util

---
+++ test/util.js
@@ -0,0 +1,18 @@
+const util = {
+  expectThrow: async promise => {
+    try {
+      let result = await promise;
+      console.log(result);
+    } catch (error) {
+      const invalidJump = error.message.search('invalid JUMP') >= 0
+      const invalidOpcode = error.message.search('invalid opcode') >= 0
+      const outOfGas = error.message.search('out of gas') >= 0
+      assert(invalidJump || invalidOpcode || outOfGas, "Expected throw, got '" + error + "' instead")
+      return
+    }
+    assert.fail('Expected throw not received')
+  }
+}
+
+
+module.exports = util;
