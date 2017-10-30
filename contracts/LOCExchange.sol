45bc02c7caa2d8f8f3fbeb767ec534a90f3883a8
George Spasov <spasov05203@gmail.com>
2017-10-30 09:11:46 +0200

    Backbones of the Exchange contract

--- contracts/LOCExchange.sol
+++ contracts/LOCExchange.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.15;
 import './Ownable.sol';
 import './Pausable.sol';
 import './Oraclized.sol';
+import './Destructible.sol';
 import './math/SafeMath.sol';
 
 /**
@@ -10,6 +11,26 @@ import './math/SafeMath.sol';
  * @dev Contract for exchanging LOC to ETH
  * Allows for exchanging LOCwei to wei
  */
-contract LOCExchange is Ownable, Pausable, Oraclized {
+contract LOCExchange is Ownable, Pausable, Destructible, Oraclized {
+
+    event LogLocExchanged(uint LocWei, uint ETHwei, uint rate);
+    event LogLocWithdrawal(uint LocWei);
+
     function LOCExchange(address initialOracle) Oraclized(initialOracle) public {}
+
+    function weiToLocWei(uint weiAmount) constant public whenNotPaused returns(uint) {
+        // Function used to find out how much Loc must be approved/allowed to get certain amount of wei
+    } 
+
+    function exchange(uint LocWei) public whenNotPaused returns(uint) {
+        // Transfer to this contract certain amount of LOCWei and send back wei to the message sender
+    }
+
+    function withdrawLOC(uint LocWeiWithdrawAmount) public whenNotPaused onlyOwner returns(uint) {
+        // Send given amount of LOC from this contract to the owner
+    }
+
+    function getLocBalance() constant public returns(uint) {
+        // Get the loc balance from the LOC token contract
+    }
 }
